e09f0b4b3de14a2655de6cc333991cf6fc6f93dd
smashism <emily@modtitan.com>
2015-03-13 17:47:04 -0500

    Update pulse_version.sh
    Using printf for cleaner results (gets rid of extraneous space) and a tidier script.

--- pulse_version.sh
+++ pulse_version.sh
@@ -7,7 +7,7 @@
 if [ -d /Applications/Junos\ Pulse.app ]; then
 	
 VERSION=`grep -o "DisplayVer.*" /Applications/Junos\ Pulse.app/Contents/Resources/JunosPulseVersion.ini | cut -d'=' -f2`
-RESULT=$(echo "${VERSION[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
+RESULT=($(printf "%s\n" "${VERSION[@]}" | sort -u));
 	
 	echo "<result>$RESULT</result>"
 	
