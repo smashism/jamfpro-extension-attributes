d4acc17ce13f85becdbfcf96d9025cfe70100c88
smashism <hellomodtitan@gmail.com>
2015-08-21 15:24:28 -0500

    Create omnigraffle6_serial_ea.sh

---
+++ omnigraffle6_serial_ea.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# by jhbush jamfnation and macadmins slack #jamfnation
+# added 2015-08-20
+#
+
+currentUser=`ls -l /dev/console | awk {' print $3 '}`
+if [ -d "/Applications/OmniGraffle.app" ]; then
+ result=`cat /Users/$currentUser/Library/Containers/com.omnigroup.OmniGraffle6/Data/Library/Application\ Support/Omni\ Group/Software\ Licenses/OmniGraffle*.omnilicense | grep -A 1 Key | grep string | sed 's/<string>//g' | sed 's/<\/string>//g' | awk '{print $1}'`
+echo "<result>$result</result>"
+else
+echo "<result>Not Installed</result>"
+fi
