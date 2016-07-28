0a2ca8237acfa8ec38a3ae7ef7f1e4f1f637f731
smashism <hellomodtitan@gmail.com>
2016-07-28 17:41:22 -0500

    Update office_licensing.sh
    Checks for VL and O365 licensing files. Will preference VL over O365 (much like Microsoft's pricing model).

--- office_licensing.sh
+++ office_licensing.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 #
-# by smashism 2016-07-19
+# script by emily k 2016-07-19
 # to detect office licensing type
 # 
 
-if [ -f "/Library/Preferences/com.microsoft.office.licensing.plist" ] ; then
+loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+
+if [ -f "/Library/Preferences/com.microsoft.office.licensing.plist" ] || [ -f "/Library/Preferences/com.microsoft.office.licensingV2.plist" ]; then
     license_type="Volume Licensed"
-else
+elif [ -f "/Users/$loggedInUser/Library/Group Containers/UBF8T346G9.Office/com.microsoft.Office365.plist" ] || [ -f "Users/$loggedInUser/Library/Application\ Support/Microsoft/Office/DRM/e0QyRjBFODBFLTM3NzYtNEFCOS1BMzVGLTc1Mjc2NzlCQUJDO" ]; then
     license_type="Office 365"
 fi
 
