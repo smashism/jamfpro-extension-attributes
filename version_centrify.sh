9afb71e139667df0acf9816c5383e81a1edd5ab4
emily <hellomodtitan@gmail.com>
2017-06-23 12:28:29 -0500

    Create version_centrify.sh

---
+++ version_centrify.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+###
+#
+#            Name:  version_centrify.sh
+#     Description:  This script is an Extension Attribute (EA) for the Casper Suite/Jamf Pro
+#                   Server. It checks the version of Centrify installed on a machine via the
+#                   MacDiagnosticTool.app and reports it. If not installed, emojis (that are tested
+#                   to load in a JSS version 9.98 or higher in Chrome. Not tested on Windows or in
+#                   Safari. We're not animals.
+#          Author:  Emily Kausalik (drkausalik@gmail.com)
+#         Created:  2017-06-23
+#
+###
+
+if [ -f "/Library/Application Support/Centrify/MacDiagnosticTool.app/Contents/Info.plist" ] ; then
+    VERSION=$( defaults read "/Library/Application Support/Centrify/MacDiagnosticTool.app/Contents/Info.plist" CFBundleShortVersionString )
+else
+    VERSION=" ❌✌️ "
+fi
+
+echo "<result>$VERSION</result>"
